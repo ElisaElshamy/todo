348827f041bc7015e5cb0f0053ad26926a0ba3df
voyevoda <elisa9el@gmail.com>
2020-10-03 00:05:44 -0400

    Added Todos state hook to Todos component

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Todos() {
-  return (
-    <div>
-      <h1>Todos</h1>
-    </div>
-  );
+  const todolist = [
+    {
+      id: 1,
+      title: "Take out the trash",
+      completed: false,
+    },
+    {
+      id: 2,
+      title: "Dinner with Raiden",
+      completed: false,
+    },
+    {
+      id: 3,
+      title: "Meeting with boss",
+      completed: false,
+    },
+  ];
+  const [todos, setTodos] = useState(todolist);
+  return todos.map((todo) => <h3>{todo.title}</h3>);
 }
 
 export default Todos;
