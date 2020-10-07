c731d78a5d616d14a3f5a209189aac84cafdb1ab
voyevoda <elisa9el@gmail.com>
2020-10-06 21:37:56 -0400

    Added delete button and styles to TodoItem component

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -31,7 +31,7 @@ const btnStyle = {
   background: "#ff0000",
   color: "#fff",
   border: "none",
-  padding: "5px 10px",
+  padding: "5px 9px",
   borderRadius: "50%",
   cursor: "pointer",
   float: "right",
