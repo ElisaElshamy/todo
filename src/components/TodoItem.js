bebb69de4f25953d02c17236f23097b551941911
voyevoda <elisa9el@gmail.com>
2020-10-06 21:37:14 -0400

    Added delete button and styles to TodoItem component

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -18,12 +18,23 @@ function TodoItem(props) {
     <div style={getStyle()}>
       <input type="checkbox" onChange={props.markComplete.bind(this, id)} />{" "}
       {title}
+      <button style={btnStyle}>x</button>
     </div>
   );
 }
 
 TodoItem.propTypes = {
   props: PropTypes.object,
+};
+
+const btnStyle = {
+  background: "#ff0000",
+  color: "#fff",
+  border: "none",
+  padding: "5px 10px",
+  borderRadius: "50%",
+  cursor: "pointer",
+  float: "right",
 };
 
 export default TodoItem;
