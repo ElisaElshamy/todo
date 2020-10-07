6a001137f40ce6bbcedc17d97acd384203340224
voyevoda <elisa9el@gmail.com>
2020-10-06 23:07:33 -0400

    Added onChange and onSubmit functions to AddTodo component

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function AddTodo() {
+export default function AddTodo(props) {
+  const todo = {
+    title: "",
+  };
+
+  const onChange = (e) => {
+    setTodoItem({ [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    props.addTodo(todoItem.title);
+    setTodoItem({ [e.target.name]: "" });
+  };
+
+  const [todoItem, setTodoItem] = useState(todo);
   return (
-    <form style={{ display: flex }}>
+    <form onSubmit={onSubmit} style={{ display: "flex" }}>
       <input
         type="text"
         name="title"
         style={{ flex: 10, padding: "5px" }}
         placeholder="Add Todo ..."
+        value={todo.title}
+        onChange={onChange}
       />
       <input type="submit" value="Submit" className="btn" style={{ flex: 1 }} />
     </form>
